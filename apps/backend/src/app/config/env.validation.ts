4f88deedc5e75dbe29d09e5848bf201d6f2404b3
Giovane Fréu <giovane.freu@gmail.com>
2025-08-28 17:51:33 -0300

    feat: add Staging environment to the Environment enum

--- apps/backend/src/app/config/env.validation.ts
+++ apps/backend/src/app/config/env.validation.ts
@@ -14,6 +14,7 @@ import { Transform } from 'class-transformer';
 enum Environment {
   Development = 'development',
   Production = 'production',
+  Staging = 'staging',
   Test = 'test',
   Docker = 'docker',
 }
