b0aee688e5d946098db983d50c94f56a50a142f9
Giovane Fréu <giovane.freu@gmail.com>
2025-08-29 19:17:09 -0300

    refactor: standardize date and currency formats in LoansList and ClientsList components
    - Updated date formatting from `short` to `shortDate` for consistency.
    - Simplified currency formatting by removing unnecessary locale specifications.

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -1,17 +1,22 @@
 import { provideHttpClient } from '@angular/common/http';
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes)
+    provideRouter(appRoutes),
   ],
 };
