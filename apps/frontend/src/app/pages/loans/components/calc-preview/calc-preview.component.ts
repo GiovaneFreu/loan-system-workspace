0b765e42e2cd1527a2654548b186f172fffd3f02
Giovane Fréu <giovane.freu@gmail.com>
2025-09-01 11:27:10 -0300

    feat: add CalcPreviewComponent for loans calculation previews
    - Created CalcPreviewComponent with initial HTML, CSS, and TypeScript setup.
    - Added component selector, template, and styles.

---
+++ apps/frontend/src/app/pages/loans/components/calc-preview/calc-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-preview',
+  standalone: false,
+  templateUrl: './calc-preview.component.html',
+  styleUrl: './calc-preview.component.css'
+})
+export class CalcPreviewComponent {
+
+}
